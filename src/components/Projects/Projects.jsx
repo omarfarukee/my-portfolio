efa6410fc5150c032710dc8e6c96baa17fcdbd6f
omarfarukee <umarfaruque1920@gmail.com>
2024-01-02 21:42:19 +0600

    my project section done

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-target-blank */
 import './Porjects.css'
 import gadget from '../../assets/image/gadget.png'
 import bike from '../../assets/image/bike.png'
@@ -6,48 +7,66 @@ import img1 from '../../assets/project img/img1.png'
 import img2 from '../../assets/project img/img2.png'
 import img3 from '../../assets/project img/img3.png'
 import img4 from '../../assets/project img/img4.png'
+import img5 from '../../assets/project img/img5.png'
+import img6 from '../../assets/project img/img6.png'
+import { FaEye } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+
 const Projects = () => {
     return (
         <div className='project-bg'>
-            <div className='flex justify-center pt-5' data-aos="fade-up">
+            <div className='flex justify-center pt-12' data-aos="fade-up">
                 <h1 className='text-3xl font-bold text-white'>My Projects</h1>
             </div>
             <div className='flex mt-16 justify-evenly' data-aos="fade-down">
                 <div className='ml-3 p-card'>
-                    <div className='p-circle' style={{'--clr':'#ffff'}}>
+                    <div className='p-circle' style={{ '--clr': '#ffff' }}>
                         <img src={gadget} alt="" className='w-40 p-logo' />
                     </div>
-                    <div className='p-content'>
-                        <h2 className='text-2xl font-bold'>Coca cola</h2>
-                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias ipsum cupiditate nihil quas ut delectus natus accusantium aliquid, ab distinctio.</p>
-                        <button className='p-3 border'><a href="">explore more</a></button>
+                    <div className='p-1 p-content'>
+                        <div className='flex gap-2 p-2'>
+                            <button className='w-32 p-1 bg-blue-900 rounded-md'><a href="https://gadget-sailor-frontend.vercel.app/" className='flex items-center justify-center gap-2 text-white' target='_blank'><FaEye />Link</a></button>
+                            <button className='w-32 p-1 bg-blue-900 rounded-md'><a href="" className='flex items-center justify-center gap-2 text-white'><FaGithub />Client Repo</a></button>
+                        </div>
+                        <div className='flex justify-center'>
+                            <button className='w-32 p-1 bg-blue-900 rounded-md'><a href="" className='flex items-center justify-center gap-2 text-white'><FaGithub />Server Repo</a></button>
+                        </div>
                     </div>
                     <img src={img1} alt="" className='p-product-img' />
                     <img src={img2} alt="" className='mt-40 p-product-img1' />
                 </div>
                 <div className='ml-3 p-card'>
-                    <div className='p-circle' style={{'--clr':'#16a34a'}}>
+                    <div className='p-circle' style={{ '--clr': '#16a34a' }}>
                         <img src={bike} alt="" className='w-40 p-logo bg-slate-200 rounded-2xl' />
                     </div>
                     <div className='p-content'>
-                        <h2 className='text-2xl font-bold'>Coca cola</h2>
-                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias ipsum cupiditate nihil quas ut delectus natus accusantium aliquid, ab distinctio.</p>
-                        <button className='p-3 border'><a href="">explore more</a></button>
+                    <div className='flex gap-2 p-2'>
+                            <button className='w-32 p-1 bg-green-700 rounded-md'><a href="https://gadget-sailor-frontend.vercel.app/" className='flex items-center justify-center gap-2 text-white' target='_blank'><FaEye />Link</a></button>
+                            <button className='w-32 p-1 bg-green-700 rounded-md'><a href="" className='flex items-center justify-center gap-2 text-white'><FaGithub />Client Repo</a></button>
+                        </div>
+                        <div className='flex justify-center'>
+                            <button className='w-32 p-1 bg-green-700 rounded-md'><a href="" className='flex items-center justify-center gap-2 text-white'><FaGithub />Server Repo</a></button>
+                        </div>
+
                     </div>
                     <img src={img3} alt="" className='p-product-img' />
                     <img src={img4} alt="" className='mt-40 p-product-img1' />
                 </div>
                 <div className='ml-3 p-card'>
-                    <div className='p-circle' style={{'--clr':'#32a19f'}}>
+                    <div className='p-circle' style={{ '--clr': '#32a19f' }}>
                         <img src={digital} alt="" className='w-40 rounded-lg p-logo' />
                     </div>
                     <div className='p-content'>
-                        <h2 className='text-2xl font-bold'>Coca cola</h2>
-                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias ipsum cupiditate nihil quas ut delectus natus accusantium aliquid, ab distinctio.</p>
-                        <button className='p-3 border'><a href="">explore more</a></button>
+                    <div className='flex gap-2 p-2'>
+                            <button className='w-32 p-1 rounded-md bg-sky-400'><a href="https://gadget-sailor-frontend.vercel.app/" className='flex items-center justify-center gap-2 text-white' target='_blank'><FaEye />Link</a></button>
+                            <button className='w-32 p-1 bg-green-700 rounded-md'><a href="" className='flex items-center justify-center gap-2 text-white'><FaGithub />Client Repo</a></button>
+                        </div>
+                        <div className='flex justify-center'>
+                            <button className='w-32 p-1 bg-green-700 rounded-md'><a href="" className='flex items-center justify-center gap-2 text-white'><FaGithub />Server Repo</a></button>
+                        </div>
                     </div>
-                    <img src={img1} alt="" className='p-product-img' />
-                    <img src={img2} alt="" className='mt-40 p-product-img1' />
+                    <img src={img5} alt="" className='p-product-img' />
+                    <img src={img6} alt="" className='mt-40 p-product-img1' />
                 </div>
             </div>
         </div>
