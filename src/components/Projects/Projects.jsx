ef97c2ab957006e23d0fe1faf35be5ca9403b245
omarfarukee <umarfaruque1920@gmail.com>
2024-01-04 15:45:50 +0600

    my projects done

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -3,12 +3,18 @@ import './Porjects.css'
 import gadget from '../../assets/image/gadget.png'
 import bike from '../../assets/image/bike.png'
 import home from '../../assets/image/homesfood.png'
+import quick from '../../assets/image/quick.png'
+import glob from '../../assets/image/campus.png'
 import img1 from '../../assets/project img/img1.png'
 import img2 from '../../assets/project img/img2.png'
 import img3 from '../../assets/project img/img3.png'
 import img4 from '../../assets/project img/img4.png'
 import img5 from '../../assets/project img/img5.png'
 import img6 from '../../assets/project img/img6.png'
+import img9 from '../../assets/project img/img9.png'
+import img10 from '../../assets/project img/img10.png'
+import img11 from '../../assets/project img/img11.png'
+import img12 from '../../assets/project img/img12.png'
 import { FaEye } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { useState } from 'react'
@@ -25,14 +31,14 @@ const frontEnd = () =>{
     setFront('blcok')
     setFull('hidden')
 }
-const [b1,setB1] = useState('w-40 p-3 font-bold rounded-lg bg-gray-950')
+const [b1,setB1] = useState('w-40 p-3 font-bold rounded-lg b1')
 const [b2,setB2] = useState('w-40 p-3  rounded-lg')
 const button1 = () =>{
     setB2('w-40 p-3 rounded-lg')
-    setB1('w-40 p-3 font-bold rounded-lg bg-gray-950')
+    setB1('w-40 p-3 font-bold rounded-lg b1')
 }
 const button2 = () =>{
-    setB2('w-40 p-3 font-bold rounded-lg bg-gray-950')
+    setB2('w-40 p-3 font-bold rounded-lg b1')
     setB1('w-40 p-3 rounded-lg')
 }
 
@@ -120,31 +126,27 @@ const button2 = () =>{
                   <div className='flex mt-16 justify-evenly'>
 
             <div className='ml-3 p-card'>
-                    <div className='p-circle' style={{ '--clr': '#7faa51' }}>
-                        <img src={home} alt="" className='w-40 rounded-lg p-logo' />
+                    <div className='p-circle' style={{ '--clr': '#b24cbf' }}>
+                        <img src={quick} alt="" className='w-40 rounded-lg p-logo' />
                     </div>
                     <div className='p-3 p-content'>
                     <h1 className='flex justify-center text-2xl font-bold text-white'>Features</h1>
                         <small className='text-white'>
-                            <li>• Here organized some courses based on 6 topics</li>
-                            <li>• User can see specific details about etch courses, React to PDF</li>
-                            <li>• User must logged in to see course details.</li>
-                            <li>• Dark mood light mood toggle button.</li>
+                            <li>• The site is comport to Visit and its UI is very <span className='ml-2'>massive .</span></li>
+                            <li>• In here we gave you Some common quiz that you <span className='ml-2'>can justify yourself in this way how you good at <span className='ml-2'>his.</span></span> </li>
+                            <li>• The site is stunning for you .</li>
                         </small>
                         <div className='flex gap-2 p-2 mt-5'>
-                            <button className='w-32 p-1 bg-green-900 rounded-md'><a href="https://homes-food.web.app/" className='flex items-center justify-center gap-2 text-white' target='_blank'><FaEye />Link</a></button>
-                            <button className='w-32 p-1 bg-green-900 rounded-md'><a href="" className='flex items-center justify-center gap-2 text-white'><FaGithub />Client Repo</a></button>
-                        </div>
-                        <div className='flex justify-center'>
-                            <button className='w-32 p-1 bg-green-900 rounded-md'><a href="" className='flex items-center justify-center gap-2 text-white'><FaGithub />Server Repo</a></button>
+                            <button className='w-32 p-1 bg-purple-900 rounded-md'><a href="https://cosmic-quick-quiz-detector-qui-55c193.netlify.app/home" className='flex items-center justify-center gap-2 text-white' target='_blank'><FaEye />Link</a></button>
+                            <button className='w-32 p-1 bg-purple-900 rounded-md'><a href="https://github.com/omarfarukee/Quick-Quiz-Detector" target='_blank' className='flex items-center justify-center gap-2 text-white'><FaGithub />Client Repo</a></button>
                         </div>
                     </div>
-                    <img src={img5} alt="" className='p-product-img' />
-                    <img src={img6} alt="" className='mt-40 p-product-img1' />
+                    <img src={img9} alt="" className='p-product-img' />
+                    <img src={img10} alt="" className='mt-40 p-product-img1' />
                 </div>
             <div className='ml-3 p-card'>
-                    <div className='p-circle' style={{ '--clr': '#7faa51' }}>
-                        <img src={home} alt="" className='w-40 rounded-lg p-logo' />
+                    <div className='p-circle' style={{ '--clr': '#0dcaf0' }}>
+                        <img src={glob} alt="" className='w-40 rounded-lg p-logo' />
                     </div>
                     <div className='p-3 p-content'>
                     <h1 className='flex justify-center text-2xl font-bold text-white'>Features</h1>
@@ -155,15 +157,13 @@ const button2 = () =>{
                             <li>• Dark mood light mood toggle button.</li>
                         </small>
                         <div className='flex gap-2 p-2 mt-5'>
-                            <button className='w-32 p-1 bg-green-900 rounded-md'><a href="https://homes-food.web.app/" className='flex items-center justify-center gap-2 text-white' target='_blank'><FaEye />Link</a></button>
-                            <button className='w-32 p-1 bg-green-900 rounded-md'><a href="" className='flex items-center justify-center gap-2 text-white'><FaGithub />Client Repo</a></button>
+                            <button className='w-32 p-1 rounded-md bg-cyan-600'><a href="https://campus-sia-396920.netlify.app/" className='flex items-center justify-center gap-2 text-white' target='_blank'><FaEye />Link</a></button>
+                            <button className='w-32 p-1 rounded-md bg-cyan-600'><a href="https://github.com/omarfarukee/campus-sia" target='_blank' className='flex items-center justify-center gap-2 text-white'><FaGithub />Client Repo</a></button>
                         </div>
-                        <div className='flex justify-center'>
-                            <button className='w-32 p-1 bg-green-900 rounded-md'><a href="" className='flex items-center justify-center gap-2 text-white'><FaGithub />Server Repo</a></button>
-                        </div>
+                    
                     </div>
-                    <img src={img5} alt="" className='p-product-img' />
-                    <img src={img6} alt="" className='mt-40 p-product-img1' />
+                    <img src={img11} alt="" className='p-product-img' />
+                    <img src={img12} alt="" className='mt-40 p-product-img1' />
                 </div>
             </div>
             </div>
