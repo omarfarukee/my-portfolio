62732ecc3cc69f54e1516f4a3e70e791fcbebda0
omarfarukee <umarfaruque1920@gmail.com>
2024-01-25 16:22:13 +0600

    contact me done

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,32 +1,32 @@
 
 import './Contact.css'
-import contact from '../../assets/contact-img/420797817_2073469626385685_3776584553687943742_n-removebg-preview.png'
 const Contact = () => {
     return (
         <div className="c-back">
             <div className='flex justify-center pt-12 mb-10 text-3xl text-white'>
                 Contact
             </div>
-            <div className='flex c-back justify-evenly'>
-                <div className='border rounded-full c-img'>
-                    <img src={contact} className='w-96' alt="" />
+            <div className='flex justify-evenly'>
+               
+                <div className='wrapper' data-aos="fade-up">
+                    <form action="">
+                        < div className='inputBox'>
+                            <input type="text" required />
+                            <span>Your email</span>
+                        </div>
+                        < div className='inputBox'>
+                            <input type="text" required />
+                            <span>Subject</span>
+                        </div>
+                        < div className='inputBox'>
+                            <input type="text" required className='h-20'/>
+                            <span>YOUR message</span>
+                        </div>
+                        <button type='submit' className='text-white c-button'>Submit</button>
+                    </form>
                 </div>
-            <div className='wrapper'>
-                <form action="">
-                    <div className='input-box'>
-                        <input type="text" placeholder='your email' />
-                    </div>
-                    <div className='input-box'>
-                        <input type="text" placeholder='Subject' />
-                    </div>
-                    <div className='input-box'>
-                        <input type="text" placeholder='you message' />
-                    </div>
-                    <button type='submit' className='text-white c-button'>Submit</button>
-                </form>
             </div>
-            </div>
-            
+
 
         </div>
     );
