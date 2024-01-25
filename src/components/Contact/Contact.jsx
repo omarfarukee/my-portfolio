ec3b8ec01d3a49cb0b90a76f04b31bb5931f81ef
omarfarukee <umarfaruque1920@gmail.com>
2024-01-25 21:49:37 +0600

    ....

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,59 +1,86 @@
+/* eslint-disable no-unused-vars */
 
-import { useRef } from 'react';
+import React, { useRef, useState } from 'react';
+import { FiLoader } from "react-icons/fi";
 import './Contact.css'
 import emailjs from '@emailjs/browser';
 import toast from 'react-hot-toast';
+
 const Contact = () => {
+  const form = useRef();
 
-    const form = useRef();
+  const [formData, setFormData] = useState({
+    from_name: '',
+    from_email: '',
+    message: ''
+  });
 
+  const [spin, setSpin] = useState('hidden');
+  
   const sendEmail = (e) => {
     e.preventDefault();
 
     emailjs.sendForm('service_2osiuyr', 'template_r78nv93', form.current, 'zX_s9wlDIJmN1qe6c')
       .then((result) => {
-        if (result){
-            toast.success("message sent successfully")
-        }else{
-            toast.error("message did not send successfully")
+        if (result) {
+          toast.success("message sent successfully");
+          location.reload();
+        } else {
+          toast.error("message did not send successfully");
         }
-          console.log(result.text);
+        console.log(result.text);
       }, (error) => {
-          console.log(error.text);
+        console.log(error.text);
       });
   };
 
-    return (
-        <div className="c-back">
-            <div className='flex justify-center pt-12 mb-10 text-3xl text-white'>
-                Contact
-            </div>
-            <div className='flex justify-evenly'>
-                <div data-aos="fade-up">
-                    <div className='wrapper' >
-                        <form action="" ref={form} onSubmit={sendEmail}>
-                            < div className='inputBox'>
-                                <input name="from_name" type="text" required />
-                                <span>Your Name</span>
-                            </div>
-                            < div className='inputBox'>
-                                <input name="from_email" type="email" required />
-                                <span>your email</span>
-                            </div>
-                            < div className='inputBox'>
-                                <input name="message" type="text" required className='h-20' />
-                                <span>YOUR message</span>
-                            </div>
-                            <button type='submit' className='text-white c-button'>Submit</button>
-                        </form>
-                    </div>
-                </div>
+  const loading = () => {
+    setSpin('block text-2xl animate-spin');
+  };
 
-            </div>
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
 
+  const isFormValid = () => {
+    return formData.from_name !== '' && formData.from_email !== '' && formData.message !== '';
+  };
 
+  return (
+    <div className="c-back">
+      <div className='flex justify-center pt-12 mb-10 text-3xl text-white'>
+        Contact
+      </div>
+      <div className='flex justify-evenly'>
+        <div data-aos="fade-up">
+          <div className='wrapper'>
+            <form action="" ref={form} onSubmit={sendEmail}>
+              <div className='inputBox'>
+                <input name="from_name" type="text" value={formData.from_name} onChange={handleChange} required />
+                <span>Your Name</span>
+              </div>
+              <div className='inputBox'>
+                <input name="from_email" type="text" value={formData.from_email} onChange={handleChange} required />
+                <span>Your Email</span>
+              </div>
+              <div className='inputBox'>
+                <input name="message" type="text" value={formData.message} onChange={handleChange} required className='h-20' />
+                <span>Your Message</span>
+              </div>
+              <button type='submit' className={`flex items-center justify-center gap-3 text-white c-button ${!isFormValid() ? 'disabled' : ''}`} onClick={loading} disabled={!isFormValid()}>
+                Submit
+                <span className={spin}><FiLoader /></span>
+              </button>
+            </form>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
