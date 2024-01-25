adcdfea5c4af1a529d46a5cf11de7814db686428
omarfarukee <umarfaruque1920@gmail.com>
2024-01-25 11:36:42 +0600

    contact page designed

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,10 +3,10 @@ import './Contact.css'
 const Contact = () => {
     return (
         <div className="c-back">
-            <div className='flex justify-center pt-12 mb-10 text-3xl'>
+            <div className='flex justify-center pt-12 mb-10 text-3xl text-white'>
                 Contact
             </div>
-            <div className='flex justify-center'>
+            {/* <div className='flex justify-center'>
                 <div>
                     <div className="mb-5 field-group">
                         <input type="text" name="name" id='name' className="input-field" placeholder="Enter Name" autoComplete="off" />
@@ -22,7 +22,24 @@ const Contact = () => {
                     </div>
                 </div>
 
+            </div> */}
+            <div className='c-back'>
+            <div className='wrapper'>
+                <form action="">
+                    <div className='input-box'>
+                        <input type="text" placeholder='your email' />
+                    </div>
+                    <div className='input-box'>
+                        <input type="text" placeholder='Subject' />
+                    </div>
+                    <div className='input-box'>
+                        <input type="text" placeholder='you message' />
+                    </div>
+                    <button type='submit' className='text-white c-button'>Submit</button>
+                </form>
             </div>
+            </div>
+            
 
         </div>
     );
