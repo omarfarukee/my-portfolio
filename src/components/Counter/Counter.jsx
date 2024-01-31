24836fa6974ccd6c6cfd6507680af2a6eb70f4fe
omarfarukee <umarfaruque1920@gmail.com>
2024-01-31 13:30:50 +0600

    counter page done

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -2,6 +2,12 @@ import { useState } from "react";
 import CountUp from "react-countup";
 import ScrollTrigger from "react-scroll-trigger";
 import './Counter.css'
+import project from '../../assets/counter-img/project.png'
+import github from '../../assets/counter-img/pngwing.com (2).png'
+import clock from '../../assets/counter-img/fast-time-clock-ui-icon-sign-symbol-3d-rendering-removebg-preview.png'
+import { GoProject } from "react-icons/go";
+import { FaGithub, FaUserClock } from "react-icons/fa";
+
 const Counter = () => {
     const [counterOn, setCounterOn] = useState(false)
     return (
@@ -10,16 +16,38 @@ const Counter = () => {
                 <div className='flex justify-center pt-12 mb-10' data-aos="fade-down">
                     <h1 className='text-3xl font-bold text-white'>Dedication</h1>
                 </div>
-                <div>
-                    <div className="flex justify-center">
-                        <div className="p-3 border rounded-lg">
-                            <h1 className="text-3xl font-bold text-white">projects</h1>
-                            <hr />
+                <div className="flex items-center justify-around p-5 lg:h-96"data-aos='fade-up'>
+                    <div className="">
+                        <div className="flex">
+                            <img src={project} className="rounded-full w-52 " alt="" />
                             {counterOn &&
-                            <p  className="flex justify-center mt-2 text-3xl font-extrabold nion"><CountUp start={0} end={100} duration={3} delay={0} />+</p>
-                        }
+                                <div className="mt-5 ml-2"><p className="mt-2 font-extrabold text-7xl nion"><CountUp start={0} end={25} duration={4} delay={0} />+</p>
+                                    <p className="flex items-center gap-2 mt-2 font-bold text-green-200 uppercase"><span className="text-lg text-green-200 animate-bounce"><GoProject /></span>Poroject Complete
+                                    </p></div>
+                            }
                         </div>
-                      
+
+                    </div>
+                    <div className="">
+                        <div className="flex">
+                            <img src={clock} className="rounded-full w-44" alt="" />
+                            {counterOn &&
+                                <div className="mt-5 ml-2"><p className="mt-2 font-extrabold text-7xl nion-2"><CountUp start={0} end={2500} duration={7} delay={0} />+</p>
+                                    <p className="flex items-center gap-2 mt-2 font-bold text-pink-300 uppercase"><span className="text-lg text-pink-300 animate-bounce"><FaUserClock/></span>Learnig Hours</p></div>
+                            }
+                        </div>
+
+                    </div>
+                    <div className="">
+                        <div className="flex">
+                            <img src={github} className="rounded-full w-36" alt="" />
+                            {counterOn &&
+                                <div className="mt-5 ml-2"><p className="mt-2 font-extrabold text-7xl nion-3"><CountUp start={0} end={905} duration={5} delay={0} />+</p>
+                                    <p className="flex items-center gap-2 mt-2 font-bold text-indigo-300 uppercase"><span 
+                                    className="text-lg text-indigo-300 animate-bounce"><FaGithub/></span>GitHub commits</p></div>
+                            }
+                        </div>
+
                     </div>
 
                 </div>
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -49,20 +49,20 @@ const SkillCard = ({ color, percentage, image, skillName }) => {
 
 const Skills = () => {
   const skills = [
-    { type: 'HTML', level: 98},
-    { type: 'CSS', level: 90},
-    { type: 'Javascript', level: 85},
+    { type: 'HTML', level: 98 },
+    { type: 'CSS', level: 90 },
+    { type: 'Javascript', level: 85 },
     { type: 'React', level: 85 },
-    { type: 'Node.js', level: 85},
-    { type: 'TailWind ', level: 90},
-    { type: 'Bootstrap', level: 92},
-    { type: 'Firebase', level: 85},
-    { type: 'Express.js', level: 80},
-    { type: 'MongoDB', level: 92},
-    { type: 'TypeScript', level:82 },
-    { type: 'R-roter', level: 88},
+    { type: 'Node.js', level: 85 },
+    { type: 'TailWind ', level: 90 },
+    { type: 'Bootstrap', level: 92 },
+    { type: 'Firebase', level: 85 },
+    { type: 'Express.js', level: 80 },
+    { type: 'MongoDB', level: 92 },
+    { type: 'TypeScript', level: 82 },
+    { type: 'R-roter', level: 88 },
     { type: 'Mongoose', level: 65 },
-    { type: 'Next.js', level: 60}
+    { type: 'Next.js', level: 60 }
   ];
   const colors = {
     bar: '#3E3232',
@@ -88,43 +88,46 @@ const Skills = () => {
   const [animationOn, setAnimationOn] = useState(false)
   return (
     <ScrollTrigger onEnter={() => setAnimationOn(true)}>
-    <div className=' skills-top skills-bg'>
-      <div className='flex justify-center pt-5' data-aos="fade-up">
-        <h1 className='text-3xl font-bold text-white'>Skills Analysis</h1>
+      <div className=' skills-top skills-bg'>
+        <div className='flex justify-center pt-5' data-aos="fade-up">
+          <h1 className='text-3xl font-bold text-white'>Skills Analysis</h1>
+        </div>
+        <div className='block mt-12 lg:hidden'>
+          <div data-aos='fade-down' className='lg:w-96'>
+            <SkillBar skills={skills} height={40} colors={colors} className='border' />
+          </div>
+        </div>
+
+
+        <div className='mt-10 skill-container'>
+          {animationOn &&
+            <div>
+              <div className='flex justify-center gap-10'>
+                <SkillCard color='#e14000' percentage='98' skillName='HTML' image={html} />
+                <SkillCard color='#595bff' percentage='90' skillName='CSS' image={css} />
+                <SkillCard color='#b583f9' percentage='92' skillName='Boostrap' image={boot} />
+                <SkillCard color='#3bb0af' percentage='90' skillName='Tailwind' image={tailwind} />
+                <SkillCard color='#fff13e' percentage='85' skillName='Java Script' image={js} />
+                <SkillCard color='#ffca28' percentage='85' skillName='Firebase' image={fire} />
+              </div>
+              <div className='flex justify-center gap-10 mt-5 mb-5'>
+                <SkillCard color='#7a78ff' percentage='85' skillName='React js' image={reacts} />
+                <SkillCard color='#8c2829' percentage='88' skillName='React Router' image={dom} />
+                <SkillCard color='#76c1e7' percentage='82' skillName='TypeScript' image={typescript} />
+                <SkillCard color='#7bb265' percentage='85' skillName='Node js' image={node} />
+                <SkillCard color='#949492' percentage='80' skillName='Express js' image={express} />
+
+
+              </div>
+              <div className='flex justify-center gap-10'>
+                <SkillCard color='#81c564' percentage='92' skillName='MongoDB' image={mongodb} />
+                <SkillCard color='#880000' percentage='75' skillName='Mongoose' image={mongoo} />
+                <SkillCard color='#d0d0d0' percentage='60' skillName='Next js' image={nexts} />
+              </div>
+            </div>
+          }
+        </div>
       </div>
-      <div className='block mt-12 lg:hidden'>
-        <div data-aos='fade-down' className='lg:w-96'>
-          <SkillBar skills={skills} height={40} colors={colors} className='border' />
-        </div>
-      </div>
-      {animationOn &&
-    
-      <div className='mt-10 skill-container' data-aos="fade-up">
-        <div className='flex justify-center gap-10'>
-          <SkillCard color='#e14000' percentage='98' skillName='HTML' image={html} />
-          <SkillCard color='#595bff' percentage='90' skillName='CSS' image={css} />
-          <SkillCard color='#b583f9' percentage='92' skillName='Boostrap' image={boot} />
-          <SkillCard color='#3bb0af' percentage='90' skillName='Tailwind' image={tailwind} />
-          <SkillCard color='#fff13e' percentage='85' skillName='Java Script' image={js} />
-          <SkillCard color='#ffca28' percentage='85' skillName='Firebase' image={fire} />
-        </div>
-        <div className='flex justify-center gap-10 mt-5 mb-5'>
-          <SkillCard color='#7a78ff' percentage='85' skillName='React js' image={reacts} />
-          <SkillCard color='#8c2829' percentage='88' skillName='React Router' image={dom} />
-          <SkillCard color='#76c1e7' percentage='82' skillName='TypeScript' image={typescript} />
-          <SkillCard color='#7bb265' percentage='85' skillName='Node js' image={node} />
-          <SkillCard color='#949492' percentage='80' skillName='Express js' image={express} />
-
-
-        </div>
-        <div className='flex justify-center gap-10'>
-          <SkillCard color='#81c564' percentage='92' skillName='MongoDB' image={mongodb} />
-          <SkillCard color='#880000' percentage='75' skillName='Mongoose' image={mongoo} />
-          <SkillCard color='#d0d0d0' percentage='60' skillName='Next js' image={nexts} />
-        </div>
-
-      </div>  }
-    </div>
     </ScrollTrigger>
   );
 };
